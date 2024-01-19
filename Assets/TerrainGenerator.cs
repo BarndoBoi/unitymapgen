19d058e4f963e05aeb4d27a595cc622f7e46d3c6
fishmonkey1 <11984660+fishmonkey1@users.noreply.github.com>
2024-01-19 16:28:00 -0500

    comments/planning :3

--- Assets/TerrainGenerator.cs
+++ Assets/TerrainGenerator.cs
@@ -20,7 +20,7 @@ public class TerrainGenerator : MonoBehaviour
 
     //Init map and prep noise for terrain layer
 
-    public float noiseScale = 0.9f;
+    public float noiseScale = 2.0f;
     public float noiseFrequency = 0.25f;
     int seed = 10;
 
@@ -55,6 +55,7 @@ public class TerrainGenerator : MonoBehaviour
         // https://alastaira.wordpress.com/2013/11/14/procedural-terrain-splatmapping/ this one is better
 
 
+        // Splatmap data is stored internally as a 3d array of floats, so declare a new empty array ready for your custom splatmap data:
         float[,,] splatmapData = new float[terrainData.alphamapWidth, terrainData.alphamapHeight, terrainData.alphamapLayers];
 
         for (int y = 0; y < terrainData.alphamapHeight; y++)
@@ -66,6 +67,7 @@ public class TerrainGenerator : MonoBehaviour
                 float x_01 = (float)x / (float)terrainData.alphamapWidth;
 
                 // Sample the height at this location (note GetHeight expects int coordinates corresponding to locations in the heightmap array)
+                //float height = terrainData.GetHeight(Mathf.RoundToInt(y_01 * terrainData.heightmapHeight), Mathf.RoundToInt(x_01 * terrainData.heightmapWidth));
                 float height = terrainData.GetHeight(Mathf.RoundToInt(y_01 * terrainData.heightmapResolution), Mathf.RoundToInt(x_01 * terrainData.heightmapResolution));
 
                 // Calculate the normal of the terrain (note this is in normalised coordinates relative to the overall terrain dimensions)
@@ -79,22 +81,53 @@ public class TerrainGenerator : MonoBehaviour
 
                 // CHANGE THE RULES BELOW TO SET THE WEIGHTS OF EACH TEXTURE ON WHATEVER RULES YOU WANT
 
+                /*
+                Ok soooo whatever weight is the highest is what it will be colored as.
+                For the following group of weights, water would be selected for that area because it got the highest score.
+                splatWeights[0] = 0.1f; //brown
+                splatWeights[1] = 0.3f; //grass
+                splatWeights[2] = 0.5f; //water
+                splatWeights[3] = 0.1f; //snow
+
+                The end goal would be:
+
+                snow (cap)          /\
+                dirt               /
+                grass           __/   
+                beach    ______/
+                water   /
+                _______/
+                      
+                */
+
                 // Texture[0] has constant influence
-                splatWeights[0] = 0.1f;
-
+                // not using this right now, but if nothing had a score more than .5 then this would be selected 
+                // brown
+                splatWeights[0] = 0.5f;
+                
+                
                 // Texture[1] is stronger at lower altitudes
-                //Clamp01 returns 0 if neg, 1 if > 1.
+                // this happens to work because flat ground (water) will always have a higher weight, but needs to be fixed for better accuracy. 
+                //grass
+                splatWeights[1] = Mathf.Clamp01((terrainData.heightmapResolution - height));
 
-                splatWeights[1] = Mathf.Clamp01((terrainData.heightmapResolution - height)); 
-                Debug.Log(Mathf.Clamp01((terrainData.heightmapResolution - height)));
+                // Texture[2] stronger on flatter terrain
+                // Note "steepness" is unbounded, so we "normalise" it by dividing by the extent of heightmap height and scale factor
+                // Subtract result from 1.0 to give greater weighting to flat surfaces
+                // not sure why we are dividing by 5 but go off queen I guess.
+                // this works for now but will probably need to be changed when we add sand beaches and go below sea level, which reminds me:
+                // TODO: set negative space so water has depth instead of being flat.
+                //water
+                splatWeights[2] = 1.0f - Mathf.Clamp01(steepness * steepness / (terrainData.heightmapResolution / 5.0f));
 
-                 // Texture[2] stronger on flatter terrain
-                 // Note "steepness" is unbounded, so we "normalise" it by dividing by the extent of heightmap height and scale factor
-                 // Subtract result from 1.0 to give greater weighting to flat surfaces
-                 splatWeights[2] = 1.0f - Mathf.Clamp01(steepness * steepness / (terrainData.heightmapResolution / 5.0f));
+                // Texture[3] increases with height but only on surfaces facing positive Z axis 
+                // TODO: when brain can math, make formula to do only if height is in top lets say 15% of heights on map
+                //snow
+                splatWeights[3] = height * Mathf.Clamp01(normal.z);
 
-                 // Texture[3] increases with height but only on surfaces facing positive Z axis 
-                 splatWeights[3] = height * Mathf.Clamp01(normal.z);
+                // god I fucking hate Unity
+                //Debug.Log("brown = " + splatWeights[0] + "|   " + "grass = " + splatWeights[1] + "|   "+ "water = " + splatWeights[2] + "|   " + "snow = " + splatWeights[3] + "|   ");
+
 
                 // Sum of all textures weights must add to 1, so calculate normalization factor from sum of weights
                 float z = splatWeights.Sum();
@@ -119,6 +152,9 @@ public class TerrainGenerator : MonoBehaviour
 
     float[,] GenerateHeights()
     {
+
+        //TODO add more layers so we can do multiple passes for more variability in the Perlin noise
+        //I think Sebastian talked about it a bit in one of his vids go find it
         noise.SetNoiseType(FastNoiseLite.NoiseType.Perlin);
         noise.SetFrequency(noiseFrequency);
         noise.SetSeed(seed);
