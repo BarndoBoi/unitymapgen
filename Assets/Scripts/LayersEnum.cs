d0d7b560566ec94e8eeb888f296b2d885205d13e
Victoria Eastep <114838742+BarndoBoi@users.noreply.github.com>
2024-01-24 14:32:37 -0500

    Add Layers Enum and Expose FastNoiseLite Parameters
    Built a struct for inputting FastNoiseLite values in from the editor and started building out the TerrainFromTilemap class.
    Getting ready to swap over to a proper branch cause I'm a silly girl. c:

---
+++ Assets/Scripts/LayersEnum.cs
@@ -0,0 +1,5 @@
+public static class LayersEnum
+{ //I was thinking of making this a proper enum, but strings aren't supported so I'm instead going for const values so no typos are made in layers
+    public const string Elevation = "Elevation";
+    public const string Moisture = "Moisture";
+}
--- Assets/Scripts/TerrainFromTilemap.cs
+++ Assets/Scripts/TerrainFromTilemap.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System.Collections;
 using ProcGenTiles;
 
-public class TerrainFromTilemap
+public class TerrainFromTilemap : MonoBehaviour
 {
     /* I want to create a map with the noise library and set a TerrainData up for rendering the backing data.
     // I'll use the same method of putting the heights in an "Elevation" layer and using that to put the islands together.
@@ -10,6 +10,52 @@ public class TerrainFromTilemap
     // It would be cool to add canals or valleys between the water regions so everything is accessable by boat. :3
     */
     [SerializeField]
+    private int width;
+    [SerializeField]
+    private int height;
 
-    public Map map = new Map();
+    private Map map;
+    [SerializeField]
+    private FastNoiseLite noise;
+    [SerializeField]
+    private FastNoiseLiteParams noiseParams = new FastNoiseLiteParams();
+
+    public void Start()
+    {
+        map = new Map(width, height);
+        ReadNoiseParams(); //Init the fastnoise class with the serialized struct values
+        GenerateHeightmap(); //Seed the tiles with a set of elevation noise
+    }
+
+    public void ReadNoiseParams()
+    {
+        if (noise == null)
+            noise = new FastNoiseLite();
+
+        noise.SetSeed(noiseParams.seed);
+        noise.SetNoiseType(noiseParams.noiseType);
+        noise.SetFractalType(noiseParams.fractalType);
+        noise.SetFractalGain(noiseParams.fractalGain);
+        noise.SetFractalLacunarity(noiseParams.fractalLacunarity);
+        noise.SetFractalOctaves(noiseParams.fractalOctaves);
+        noise.SetFrequency(noiseParams.frequency);
+    }
+
+    public void GenerateHeightmap()
+    {
+        if (map.Width != width || map.Height != height)
+        {
+            //The map sizes have changed and we should regenerate the backing map sizes
+            map = new Map(width, height);
+        }
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            { //Inner for loop does most of the heavy lifting
+                Tile t = map.Tiles[i, j]; //Get the tile at the location
+
+            }
+        }
+    }
+
 }
