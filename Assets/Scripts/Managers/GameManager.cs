f6baaf7ad978feb3bd8607c06163c920d2c23c90
fishmonkey1 <11984660+fishmonkey1@users.noreply.github.com>
2024-01-27 12:57:42 -0500

    init commit, nothing connected yet

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // so we can interact from anywhere in the game
+    public static GameManager Instance;
+
+    public GameState State;
+
+    public static event Action<GameState> OnGameStateChanged;
+
+    private void Awake()
+    {
+        //TODO: read more into the Singleton pattern :3
+        //basically want there to only be only one instance
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        //TODO Change to MenuScreen once menu is made 
+        // for now just on move state
+        UpdateGameState(GameState.Move);
+    }
+
+    public void UpdateGameState(GameState newState)
+    {
+        State = newState;
+
+        switch (newState)
+        {
+            case GameState.MenuScreen:
+                HandleMenuScreen();
+                break;
+            case GameState.Move:
+                HandleMove();
+                break;
+            case GameState.Aim:
+                HandleAim();
+                break;
+            case GameState.Fire:
+                HandleFire();
+                break;
+        }
+
+        OnGameStateChanged(newState);
+    }
+
+    private void HandleMenuScreen()
+    {
+
+    }
+
+    private void HandleMove()
+    {
+
+    }
+
+    private void HandleAim()
+    {
+
+    }
+
+    private void HandleFire()
+    {
+
+    }
+
+
+
+}
+
+    public enum GameState
+{
+    MenuScreen,
+    Move,
+    Aim,
+    Fire
+}
