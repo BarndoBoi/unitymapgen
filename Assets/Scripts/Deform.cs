82a57508fa344694a896e6e1e87c8a4a34f1d327
Victoria Eastep <114838742+BarndoBoi@users.noreply.github.com>
2024-02-02 16:13:16 -0500

    Stub of terrain deformation (#9)
    Stub Terrain deformation and tweak terrain asset

---
+++ Assets/Scripts/Deform.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deform : MonoBehaviour
+{
+    /* Contains information on how large the height deformation will be
+    // Stores the radius of terrain that will be affected
+    // Use Manhattan distance to calculate amount of falloff on the height to get craters
+    // Update Elevation layer and signal that the Terrain needs to be redrawn
+    // A bit of pre-emptive optimizing since larger meshes may take a long time to recalculate (TEST TEST TEST!!)
+    */
+
+    MapLayers map;
+}
