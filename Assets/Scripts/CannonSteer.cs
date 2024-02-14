9eee7c9ffb94208b09327057e5197de11aba2a85
fishmonkey1 <11984660+fishmonkey1@users.noreply.github.com>
2024-02-14 14:31:29 -0500

    Ok, got the CannonSteer completely working.

--- Assets/Scripts/CannonSteer.cs
+++ Assets/Scripts/CannonSteer.cs
@@ -13,15 +13,11 @@ public class CannonSteer : MonoBehaviour
     [SerializeField]
     float minimumInput = 0.01f; //Can't go slower than this
 
-    
-
     private Vector2 steerInput;
 
     public GameObject cannon_tube;
     public GameObject cannon_base;
     public GameObject projectile;
-    //Rigidbody projectile_rb; <-- was using this for mass
-    //public GameObject FirePoint;
 
     // Line Render
     
@@ -51,17 +47,10 @@ public class CannonSteer : MonoBehaviour
     private float timeIntervalInPoints = 0.1f;
 
  
-
-
     // Start is called before the first frame update
     void Start()
     {
-        
-        
-        //FirePoint = GameObject.Find("FirePoint");
-
-        // Number of points to represent the trajectory
-        //trajectoryRenderer.positionCount = 100;   
+ 
     }
 
     // Update is called once per frame
@@ -89,75 +78,31 @@ public class CannonSteer : MonoBehaviour
         //apply force
         Rigidbody projectile_rb = projectile_copy.GetComponent<Rigidbody>();
 
-        projectile_rb.AddForce(cannon_tube.transform.forward * launchForce, ForceMode.Impulse);
-
-        
+        projectile_rb.AddForce(FirePoint.transform.forward * launchForce, ForceMode.Impulse);
     }
 
-    void OnChangeCamera(InputValue value)
-    {
-
-    }
-
-
+    
+    //make sure that FirePoint is set to use world space in the editor or this won't work
     private void SimulateTrajectory()
     {
-        Vector3 origin = FirePoint.position;
-        //Vector3 startVelocity = launchForce * launchPoint.up; //original
-        Vector3 startVelocity = launchForce * FirePoint.forward;  //edited
-        LineRenderer.positionCount = LinePoints;
-        float time = 0;
-        for (int i = 0; i < LinePoints; i++)
+        LineRenderer.positionCount = Mathf.CeilToInt(LinePoints / timeIntervalInPoints) + 1;
+        Vector3 startPosition = FirePoint.position; 
+        Vector3 startVelocity = launchForce * FirePoint.forward;
+
+        int i = 0;
+        LineRenderer.SetPosition(i, startPosition);
+        for (float time = 0; time < LinePoints; time += timeIntervalInPoints)
         {
-            // s = u*t + 1/2*g*t*t
-            var x = (startVelocity.x * time) + (Physics.gravity.x / 2 * time * time);
-            var y = (startVelocity.y * time) + (Physics.gravity.y / 2 * time * time);
-            Vector3 point = new Vector3(x, y, 0);
-            LineRenderer.SetPosition(i, origin + point);
-            time += timeIntervalInPoints;
+            i++;
+            Vector3 point = startPosition + time * startVelocity;
+            point.y = startPosition.y + startVelocity.y * time + (Physics.gravity.y / 2f * time * time);
+
+            LineRenderer.SetPosition(i, point);
         }
     }
 
 
 
 
-    //    private void SimulateTrajectory2()
-    //{
-    //    //LineRenderer.positionCount = Mathf.CeilToInt(LinePoints / TimeBetweenPoints) + 1;
-    //    //LineRenderer.positionCount = LinePoints;
-       
-    //    Vector3 startPosition = FirePoint.position; //this is wrong somehow :(
-    //    //Debug.Log(startPosition);
-    //    Vector3 startVelocity = launchForce * FirePoint.forward;
-
-    //    int i = 0;
-    //    LineRenderer.SetPosition(i, startPosition);
-    //    for (float time = 0; time < LinePoints; time += TimeBetweenPoints)
-    //    {
-    //        i++;
-    //        Vector3 point = startPosition + time * startVelocity;
-    //        point.y = startPosition.y + startVelocity.y * time + (Physics.gravity.y / 2f * time * time);
-
-    //        LineRenderer.SetPosition(i, point);
-    //    }
-    //}
-
-
-
-
-    //for (int i = 0; i < trajectoryPoints.Length; i++)
-    /*{
-         // Time interval for trajectory calculation
-        //float time = i * Time.fixedDeltaTime;
-
-        // need do be using init veloc here not launch force
-        float x = launchForce * time * Mathf.Cos(launchAngleRadians);
-        float y = (launchForce * time * Mathf.Sin(launchAngleRadians)) - 0.5f * (mass * g * time * time);
-        trajectoryPoints[i] = new Vector3(x, y, 0f); // Store the calculated point
-    }*/
-
-    //https://youtu.be/8mGZBYsSXcQ line render
-
-    //trajectoryRenderer.SetPositions(trajectoryPoints); // Update the Line Renderer positions
 }
 
