c251070e8b1765bb0a4d6a358b88197e16dc4475
Victoria Eastep <114838742+BarndoBoi@users.noreply.github.com>
2024-02-29 15:10:22 -0500

    Update Pathfinding and Biomes
    Getting things in place to mark regions on the map

--- Assets/Scripts/Biomes.cs
+++ Assets/Scripts/Biomes.cs
@@ -10,7 +10,20 @@ public class Biomes : MonoBehaviour
         public string name;
         public float value;
         public int index;
+        public bool isWaterLayer; //Marks if this value should be used in land/water checking
     }
        
     public List<IndexValue> AllBiomes = new List<IndexValue>();
+
+    public IndexValue GetWaterLayer()
+    {
+        for (int i = 0; i < AllBiomes.Count; i++)
+        {
+            if (AllBiomes[i].isWaterLayer)
+            {
+                return AllBiomes[i];
+            }
+        }
+        return null; //Also need to throw an error
+    }
 }
